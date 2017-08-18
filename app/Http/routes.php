ff5f51b6e3b62739215ccb2bc816f86fd5429ce7
Daniil Zavyalov <daniilzavup@gmail.com>
2017-08-18 11:01:44 +0200

    admin panel initialized

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,8 +9,13 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
+localhost/Education/public/
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
